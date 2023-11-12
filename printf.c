f2318be7d09460ff5e79c2a9ff48c805487e5c14
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-12 09:33:50 -0800

    Update

--- printf.c
+++ printf.c
@@ -4,3 +4,26 @@ int _printf(const char *format, ...)
 {
 	int char_print = 0;
 	va_list mylist;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(mylist, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_print++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+			{
+				break
+			}
+		
