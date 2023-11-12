9822e38e98f8709a0c38ee91109d94acc1612d8b
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-12 09:59:47 -0800

    Update

--- printf.c
+++ printf.c
@@ -26,4 +26,47 @@ int _printf(const char *format, ...)
 			{
 				break
 			}
-		
+
+			if (*format == '%')
+			{
+				write(1,format,1);
+				char_print++;
+			}
+			else if (*format == 'c')
+			{
+				char c  = va_arg(mylist, int);
+				write(1, &c, 1);
+				char_print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(mylist, char*);
+				int str_len = 0;
+
+				while(str[str_len] != '\0'
+				str++;
+
+
+			write(1, str, str_len);
+			char_print += str_len;
+			}
+			}
+			format++;
+			}
+			va_end(mylist);
+
+			return (char_print);
+			}
+
+int main()
+{
+_printf("Ntoko\n");
+_printf("%c\n", 'g');
+_printf("%s\n", "string");
+_printf("%%\n",);
+
+return (0);
+
+}
+
+
