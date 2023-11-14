7649ce6ae8722d7da54abaf3711c840bff7b053a
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2023-11-14 10:39:34 -0800

    Added struct for flag handling

--- handle_format.c
+++ handle_format.c
@@ -13,12 +13,15 @@ int handle_format(char specifier, va_list args, int *count)
         case '%':
             *count += print_percent();
             break;
+	case 'b':
+	    *count += *print_binary(va_arg(args, unsigned int));
+	    break;
         case 'd':
         case 'i':
             *count += print_int(va_arg(args, int));
             break;
         default:
-            return 0;  /*Unsupported format specifier*/
+            return (0);  /*Unsupported format specifier*/
     }
-    return 1;
+    return (1);
 }
--- main.h
+++ main.h
@@ -6,11 +6,39 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @add: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int add;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct stdHandler - chooses the right function depending
+ * on the specifier passed to _printf()
+ * @s: specifier
+ * @ptr: pointer to the correct printing function
+ */
+
+typedef struct printHandler
+{
+	char s;
+	int (*ptr)(va_list args, flags_t *ptr);
+} ph;
+
+
 char print_char(char character);
 int print_str(const char *str);
 char print_percent(void);
 int print_int(int n);
 int handle_format(char specifier, va_list args, int *count);
-
+char *print_binary(unsigned int num);
 
 #endif /* MAIN_H */
