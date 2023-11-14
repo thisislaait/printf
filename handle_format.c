5db6101cb39f22c038d15e435b1cf170ad0b1ff3
thisislaait <ogadi.ugo@gmail.com>
2023-11-14 10:56:28 -0800

    betty fixed

--- handle_format.c
+++ handle_format.c
@@ -2,23 +2,23 @@
 
 int handle_format(char specifier, va_list args, int *count)
 {
-    switch (specifier)
-    {
-        case 'c':
-            *count += print_char(va_arg(args, int));
-            break;
-        case 's':
-            *count += print_str(va_arg(args, char*));
-            break;
-        case '%':
-            *count += print_percent();
-            break;
-        case 'd':
-        case 'i':
-            *count += print_int(va_arg(args, int));
-            break;
-        default:
-            return 0;  /*Unsupported format specifier*/
-    }
-    return 1;
+	switch (specifier)
+	{
+		case 'c':
+			*count += print_char(va_arg(args, int));
+			break;
+		case 's':
+			*count += print_str(va_arg(args, char*));
+			break;
+		case '%':
+			*count += print_percent();
+			break;
+		case 'd':
+		case 'i':
+			*count += print_int(va_arg(args, int));
+			break;
+		default:
+			return (0);  /*Unsupported format specifier*/
+	}
+	return (1);
 }
