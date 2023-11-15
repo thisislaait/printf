9f93a02ec7466ae5fa573c1ff98e16f3a3bd1912
thisislaait <ogadi.ugo@gmail.com>
2023-11-14 19:14:32 -0800

    binsry fixed

--- print_binary.c
+++ print_binary.c
@@ -1,30 +1,11 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <stdlib.h>
 
-/**
- *print_binary :  the unsigned int argument is converted to binary
- *@arg: an unsigned integer
- *Return: null or the binary form of arg
- */
-
-char *print_binary(unsigned int num) 
-{
-	char *binary;
-	int i = 0;
-	
-	binary = malloc(sizeof(char) * (sizeof(unsigned int) * CHAR_BIT + 1)); /*assigns space in memory to store length of int + null byte*/
-	if (binary == NULL) 
-	{
-    	return (NULL);
-  	}
-	while (num > 0) 
-	{
-    	binary[i++] = (num & 1) + '0';  /* checks if int is odd and then saves the remainder to binary*/
-    	num >>= 1;
-  	}
-  	binary[i] = '\0';
-  	return (binary);
+int print_binary(unsigned int value) {
+    int count = 0;
+    if (value > 1) {
+        count += print_binary(value / 2);
+    }
+    putchar(value % 2 + '0');
+    return count + 1;
 }
