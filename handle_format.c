c4cbd36a874dadbc6cef6bcc63095cd6b1a4ce20
thisislaait <ogadi.ugo@gmail.com>
2023-11-14 16:29:11 -0800

    files removed

--- handle_format.c
+++ handle_format.c
@@ -2,7 +2,6 @@
 
 int handle_format(char specifier, va_list args, int *count)
 {
-	flags_t flags;
 	switch (specifier)
 	{
 		case 'c':
@@ -20,18 +19,6 @@ int handle_format(char specifier, va_list args, int *count)
 		case 'd':
 		case 'i':
 			*count += print_int(va_arg(args, int));
-			break;
-		case 'u':
-			*count += print_unsigned(args, &flags);
-			break;
-		case 'o':
-			*count += print_octal(args, &flags);
-			break;
-		case 'x':
-			*count += print_hex_lower(args, &flags);
-			break;
-		case 'X':
-			*count += print_hex_upper(args, &flags);
 			break;
 		default:
 			return (0);  /*Unsupported format specifier*/
