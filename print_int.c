f21f7d1e089b78baa81f76b54af85261cd307986
thisislaait <ogadi.ugo@gmail.com>
2023-11-16 22:15:36 +0100

    fix: refactored print int

--- print_int.c
+++ print_int.c
@@ -2,36 +2,45 @@
 #include <limits.h>
 
 /**
- * print_int - Handles %d and %i specifiers
- * @args: The va_list of arguments
+ * print_number - Prints an integer (helper function for print_int)
+ * @n: The integer to be printed
  * Return: The number of characters printed
  */
-int print_int(va_list args)
+int print_number(int n)
 {
-    int num = va_arg(args, int);
     int count = 0;
 
-    /* Handle negative numbers */
-    if (num < 0)
+    /* Handle the case when n is 0 */
+    if (n == 0)
+    {
+        _putchar('0');
+        count++;
+        return (count);
+    }
+
+    /* Handle the case when n is INT_MIN */
+    if (n == INT_MIN)
+    {
+        count += print_number(n / 10); /* Print the rest of the digits recursively */
+        _putchar('8'); /* Print the last digit of INT_MIN (8) separately */
+        count++;
+        return (count);
+    }
+
+    /* Handle other cases */
+    if (n < 0)
     {
         _putchar('-');
         count++;
-        /* Avoid overflow when negating INT_MIN */
-        if (num == INT_MIN)
-        {
-            count += print_number(-(num + 1)); /* Negate (num + 1) to avoid overflow */
-            _putchar('8'); /* Print the last digit of INT_MIN (8) separately */
-            count++;
-            return (count);
-        }
-        else
-        {
-            num = -num;
-        }
+        n = -n;
     }
 
-    /* Print the number */
-    count += print_number(num);
+    /* Recursively print digits */
+    if (n / 10 != 0)
+        count += print_number(n / 10);
+
+    _putchar(n % 10 + '0');
+    count++;
 
     return (count);
 }
