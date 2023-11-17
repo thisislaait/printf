d7d0601f31c8febffd8b2fa93b67811494a0669b
thisislaait <ogadi.ugo@gmail.com>
2023-11-17 03:29:14 -0800

    beetty fix

--- _printf.c
+++ _printf.c
@@ -3,18 +3,15 @@
 
 /**
  * _printf - Custom implementation of the printf function.
- * This function produces output according to a format. It supports the conversion specifiers.
- * 
+ * @format: The format string containing conversion specifiers
  * Return: The number of characters printed. Returns -1 on failure.
  */
-
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
-	
-	va_start(args, format);
 
+	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%' && handle_format(*(format + 1), args, &count))
