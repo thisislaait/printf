ded61c40a23678d8928c16379d1336d7cdf44f07
thisislaait <ogadi.ugo@gmail.com>
2023-11-17 12:53:01 +0100

    updated

--- print_percent.c
+++ print_percent.c
@@ -3,7 +3,7 @@
 /**
  * print_percent - Handles %% specifier
  * @count: Pointer to the count of characters printed
- * Return: 1 if specifier is handled, 0 otherwise
+ * Return: 0 if specifier is handled, 0 otherwise
  */
 int print_percent(int *count)
 {
