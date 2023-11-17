c75546a5b126dd88252f3fce6086514f60cf2521
thisislaait <ogadi.ugo@gmail.com>
2023-11-17 02:55:23 -0800

    betty fix

--- print_int.c
+++ print_int.c
@@ -8,26 +8,23 @@
  */
 int print_int(int n)
 {
-    int count = 0;
-
-    /* Handle negative numbers */
-    if (n < 0)
-    {
-        count += _putchar('-');
-        n = -n;
-    }
-
-    /* Handle single-digit numbers */
-    if (n < 10)
-    {
-        count += _putchar(n + '0');
-    }
-    else
-    {
-        /* Recursively print each digit */
-        count += print_int(n / 10);
-        count += _putchar(n % 10 + '0');
-    }
-
-    return count;
+	int count = 0;
+	/* Handle negative numbers */
+	if (n < 0)
+	{
+		count += _putchar('-');
+		n = -n;
+	}
+	/* Handle single-digit numbers */
+	if (n < 10)
+	{
+		count += _putchar(n + '0');
+	}
+	else
+	{
+		/* Recursively print each digit */
+		count += print_int(n / 10);
+		count += _putchar(n % 10 + '0');
+	}
+	return (count);
 }
